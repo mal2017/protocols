f2f05df95412c9240cd415373684bf285cd0e572
Matt Lawlor <matt.a.lawlor@gmail.com>
2018-11-27 10:11:36 -0500

    stuff

--- compile_and_commit.sh
+++ compile_and_commit.sh
@@ -1,8 +1,5 @@
 COMMIT_MESSAGE=$1
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book',new_session = T)" && \
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook',new_session = T)"
-wait
 
 git add . && \
 git commit -m "${COMMIT_MESSAGE}"
