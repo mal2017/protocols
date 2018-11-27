0e582833674d1606dc1e60e90279be4a719f5763
Matt Lawlor <matt.a.lawlor@gmail.com>
2018-11-27 10:08:36 -0500

    add chiprx

--- compile_and_commit.sh
+++ compile_and_commit.sh
@@ -5,7 +5,9 @@ Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook',new_session =
 wait
 
 git add . && \
-git commit -m "${COMMIT_MESSAGE}" && \
+git commit -m "${COMMIT_MESSAGE}"
+wait
+
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book',new_session = T)" && \
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook',new_session = T)" && \
 git push
