3a2c2675891f1d6c75fc51fda237a5a04eda85e9
Matt Lawlor <matt.a.lawlor@gmail.com>
2018-11-28 09:28:01 -0500

    fix block buff recipe

--- compile_and_commit.sh
+++ compile_and_commit.sh
@@ -4,7 +4,18 @@ COMMIT_MESSAGE=$1
 git add . && \
 git commit -m "${COMMIT_MESSAGE}"
 wait
+sleep 2
+
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book',new_session = T)" && \
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook',new_session = T)" && \
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook',new_session = T)"
+wait
+sleep 2
+
+git add . && \
+git commit -m "${COMMIT_MESSAGE}"
+
+wait
+sleep 2
+
 git push
